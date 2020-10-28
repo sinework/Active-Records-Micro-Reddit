bd9d38cca9e82ca40ae223843f17f6341bf3061f
sinework <sine.s.amare@gmail.com>
2020-10-28 16:54:56 +0300

    Add: user model creation and validation implementation

---
+++ app/models/user.rb
@@ -0,0 +1,5 @@
+class User < ApplicationRecord
+    validates :username, presence: true, uniqueness: true, length: { in: 3..12 }
+    validates :email, presence: true
+    validates :password, presence: true, format: { with: /\A(?=.{8,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[[:^alnum:]])/x,  message: "Atleast one capital letter, one small letter, one special character, one number and alteast eight characters are required"}
+end
