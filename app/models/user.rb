4415da631f70aad3d442da5bc823bdb83689529f
Marco <marcoerhardt95@gmail.com>
2020-10-29 09:59:01 +0100

    add comment model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
     validates :username, presence: true, uniqueness: true, length: { in: 3..12 }
     validates :email, presence: true
     validates :password, presence: true, format: { with: /\A(?=.{8,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[[:^alnum:]])/x,  message: "Atleast one capital letter, one small letter, one special character, one number and alteast eight characters are required"}
+    has_many :posts, dependent: :destroy
+    has_many :comments, dependent: :destroy
 end
